c0e3b632afe80aba46a2fdf970b208d2daccd002
krishnagopalan <51943914+krishnagopalan@users.noreply.github.com>
2019-07-03 16:35:09 +0530

    Update nkp9.py

--- nkp9.py
+++ nkp9.py
@@ -1,6 +1,6 @@
-n,k=map(int,input().split())
-q=list(map(int,input().split()))
-p=0
-for i in range(k):
-    p=p+q[i]
-print(p)
+n1,k2=map(int,input().split())
+l=list(map(int,input().split()))
+sum=0
+for i in range(k2):
+  sum=sum+l[i]
+print(sum)
